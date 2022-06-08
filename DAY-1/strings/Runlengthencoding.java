5bc209842d1fab102999fb4e11ee70e092412dd0
Gaurav <73377793+GPGPgauravpunetha12@users.noreply.github.com>
2022-06-08 22:55:14 +0530

    8

--- DAY-1/strings/Runlengthencoding.java
+++ DAY-1/strings/Runlengthencoding.java
@@ -1,43 +1,53 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class Runlengthencoding {
     public static void main(String[] args) {
-        String str="aabbcc";
-        ArrayList<String> subSeqList = new ArrayList<>();
-        int len = str.length();
-        int count=0;
-        int j=0;
-        String temp ;
-        for (int i = 0; i < len; i++) {
-            char firstChar = str.charAt(i);
+       
+//         ArrayList<String> subSeqList = new ArrayList<>();
+//         int len = str.length();
+//         int count=0;
+//         int j=0;
+//         String temp ;
+//         for (int i = 0; i < len; i++) {
+//             char firstChar = str.charAt(i);
            
-            if (subSeqList.size() == 0) {
-              //  subSeqList.add(""); // Not Include it     [, a, b, ab, c, ac, bc, abc, d, ad, bd, abd, cd, acd, bcd, abcd]
+//             if (subSeqList.size() == 0) {
+//               //  subSeqList.add(""); // Not Include it     [, a, b, ab, c, ac, bc, abc, d, ad, bd, abd, cd, acd, bcd, abcd]
                 
-              subSeqList.add("" + firstChar);// Include it
+//               subSeqList.add("" + firstChar);// Include it
                 
-            }
+//             }
          
        
-            if(str.charAt(i)==str.charAt(j)){ 
-                
-                count++;
-            if(count!=0)
-              {
-                  i++;
-                  j++;
-              }}
+//             if(str.charAt(i)==str.charAt(j)&&i<len)
+//               count++;
+       
 
-            String s=String.valueOf(count);
-          //  int subLen = subSeqList.size();
+//             String s=String.valueOf(count);
+//           //  int subLen = subSeqList.size();
     
-            for ( j = 0; j <s.length(); j++) {
+//             for ( j = 0; j <s.length(); j++) {
             
-            temp = firstChar+s;
-            subSeqList.add(temp); 
-            }
+//             temp = firstChar+s;
+//             subSeqList.add(temp); 
+//             }
           
- }
-    System.out.print(subSeqList);
+//  }
+//     System.out.print(subSeqList);
+String str="aabbcc";
+int n = str.length();
+for (int i = 0; i < n; i++) {
+
+    // Count occurrences of current character
+    int count = 1;
+    while (i < n - 1
+           && str.charAt(i) == str.charAt(i + 1)) {
+        count++;
+        i++;
     }
+
+    // Print character and its count
+    System.out.print(str.charAt(i) + "" + count);
+}
+}
 }
