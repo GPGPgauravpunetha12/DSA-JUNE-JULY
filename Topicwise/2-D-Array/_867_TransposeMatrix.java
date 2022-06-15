d56f6b07afa6c4b939e4983c8916e37b128be399
Gaurav <73377793+GPGPgauravpunetha12@users.noreply.github.com>
2022-06-15 22:17:12 +0530

    2darray

---
+++ Topicwise/2-D-Array/_867_TransposeMatrix.java
@@ -0,0 +1,19 @@
+public class _867_TransposeMatrix {
+    public static int[][] transpose(int[][] matrix) {
+        int mat[][]=new int[matrix[0].length][matrix.length];//COL,ROW
+        for (int i = 0; i < matrix.length; i++) {
+         for (int j = 0; j < matrix[0].length; j++) {
+             mat[j][i]=matrix[i][j];//COL,Row=Row,col
+             
+         }
+     }
+     
+     return mat;      
+ }
+        
+    }
+    public static void main(String[] args) {
+        int arr[][] ={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(transpose(arr));
+    }
+}
---
+++ Topicwise/2-D-Array/spiral_54.java
@@ -0,0 +1,3 @@
+public class spiral_54 {
+    
+}
